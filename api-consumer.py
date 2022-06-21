5a690ef8cd3c05d6e0a47e5e340cb10dae63d48d
Marcos Vinicius <marcosab.vinicius@gmail.com>
2022-06-21 02:04:16 -0300

    examples of functions and api consumer

--- api-consumer.py
+++ api-consumer.py
@@ -1,7 +1,12 @@
 import requests
+import json
 
 def buscar_cep():
     request = requests.get("http://viacep.com.br/ws/01001000/json/")
     print(request.content)
+    all_fields = json.loads(request.content)
+    print(all_fields)
+    print(all_fields[0]['cep'])
 
-buscar_cep()
+if __name__ == '__main__':
+    buscar_cep()
