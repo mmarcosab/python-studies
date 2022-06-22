087a158b6b44ff2a675b0448622afff7926fd646
Marcos Vinicius <marcosab.vinicius@gmail.com>
2022-06-22 18:18:43 -0300

    examples of decimal

---
+++ decimal-example.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+
+value_one = Decimal(100.78)
+value_two = Decimal(20.76)
+
+print(value_one + value_two)
+print(value_one * value_two)
+
+value_one = Decimal('100.78')
+value_two = Decimal('20.76')
+
+print(value_one + value_two)
+print(value_one * value_two)
